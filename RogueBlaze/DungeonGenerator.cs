81a04ca5c5c194661609ed2f5c0c3659ab621801
segfault0x61 <torresj1997@outlook.com>
2023-03-13 11:16:29 -0500

    basic indoor and dungeon generator

---
+++ RogueBlaze/DungeonGenerator.cs
@@ -0,0 +1,36 @@
+﻿namespace RogueBlaze
+{
+    public class DungeonGenerator
+    {
+        private string[] prefixes = { "ground" };
+
+        private string[] middles =
+        {
+            "crusted",
+            "dirt_brown",
+            "dirt_dark",
+            "grass",
+            "grass_burnt",
+            "sand"
+        };
+
+        public MapPosition[,] Map { get; }
+
+        public DungeonGenerator(int width, int height)
+        {
+            Map = new MapPosition[width, height];
+            for (var i = 0; i < width; i++)
+            for (var j = 0; j < height; j++)
+                Map[i, j] = new MapPosition
+                {
+                    X = i,
+                    Y = j,
+                    ImageName = "floor_extra_11"
+                };
+        }
+
+        public void Generate()
+        {
+        }
+    }
+}
