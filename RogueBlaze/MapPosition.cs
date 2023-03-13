b85abba9120e12d7eb71b7d9a8e7207ae8dac656
segfault0x61 <torresj1997@outlook.com>
2023-03-13 10:52:54 -0500

    Add project files.

---
+++ RogueBlaze/MapPosition.cs
@@ -0,0 +1,9 @@
+﻿namespace RogueBlaze
+{
+    public class MapPosition
+    {
+        public string ImageName { get; set; } = string.Empty;
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
